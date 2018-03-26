c519a18c459b8e9e5e6b5978e15d7618833c6183
Marbin Tan <ayamaru@gmail.com>
2018-03-26 08:54:58 -0700

    Add more build arguments and link flags from MSDN

--- build.bat
+++ build.bat
@@ -26,14 +26,14 @@ REM TODO: we'll need to port over glew32.dll and SDL2.dll SDL2_ttf.dll
 rem mkdir ..\..\build_game
 rem pushd ..\..\build_game
 
-cl /Zi /EHsc %INCLUDE_PATH% %FILES% /link /SUBSYSTEM:CONSOLE %LIB_PATH% %LIBS% 
+cl /Zi /EHsc -Gm -GS -Gs9999999 %INCLUDE_PATH% %FILES% /link -HEAP:4294967296 /SUBSYSTEM:CONSOLE %LIB_PATH% %LIBS%
 if NOT %errorlevel% == 0 goto :error
 
 
 :update_and_render
 REM build render as a dll
-set COMPILER_FLAGS=-O2 -MTd -Gm- -GR- -EHa- -Zo -Oi -WX -W4 -FC -Z7 /EHsc
-set LINKER_FLAGS= -incremental:no -opt:ref User32.lib Gdi32.lib winmm.lib Opengl32.lib glew32.lib glu32.lib SDL2main.lib SDL2_ttf.lib SDL2.lib
+set COMPILER_FLAGS=-O2 -MTd -Gm- -GR- -EHa- -Zo -Oi -WX -W4 -FC -Z7 -GS -Gs9999999 /EHsc
+set LINKER_FLAGS=-HEAP:4294967296 -incremental:no -opt:ref User32.lib Gdi32.lib winmm.lib Opengl32.lib glew32.lib glu32.lib SDL2main.lib SDL2_ttf.lib SDL2.lib
 SET INCLUDE_PATH=/I D:\Libraries\glew-1.13.0\include /I D:\Libraries\glm
 SET INCLUDE_PATH=/I D:\Libraries\SDL2-2.0.7\include %INCLUDE_PATH%
 SET INCLUDE_PATH=/I D:\Libraries\SDL2_ttf-2.0.14 %INCLUDE_PATH%
