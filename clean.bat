cd357cdd898522d5c3590c2e629ef7f903758158
Marbin Tan <ayamaru@gmail.com>
2018-04-12 13:18:33 -0700

    cmake: decouple hard coded values from cmakelist
    Add conditions for unix vs win32 in cmake
    
    temporarily set the PATHs in the "make.bat"

--- clean.bat
+++ clean.bat
@@ -1,1 +1,3 @@
 DEL *.pdb
+DEL CMakeCache.txt
+DEL main.vcxproj* main.pdb main.lib main.exp main.ilk main.exe main.dir
--- make.bat
+++ make.bat
@@ -1,3 +1,4 @@
+cmake -G "Visual Studio 14 2015 Win64" -D CMAKE_PREFIX_PATH="D:/Libraries/glew-1.13.0/include;D:/Libraries/SDL2-2.0.7/include;D:/Libraries/SDL2_ttf-2.0.14" -D CMAKE_LIBRARY_PATH="D:/Libraries/glew-1.13.0/lib/Release/x64;D:/Libraries/SDL2-2.0.7/VisualC/x64/Release;D:/Libraries/SDL2_ttf-2.0.14/VisualC/x64/Debug" .
 devenv main.vcxproj /build
 
 if NOT %errorlevel% == 0 goto :error
