4fd5c0868b4b23f4854126873413c4570a09952c
Marbin Tan <ayamaru@gmail.com>
2018-04-25 17:15:42 -0700

    Add more typedef

--- math.h
+++ math.h
@@ -13,13 +13,36 @@ typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
 
+typedef uint8  u8;
+typedef uint16 u16;
+typedef uint32 u32;
+typedef uint64 u64;
+
 typedef int8_t int8;
 typedef int16_t int16;
 typedef int32_t int32;
 typedef int64_t int64;
 
+typedef int8  s8;
+typedef int16 s16;
+typedef int32 s32;
+typedef int64 s64;
+
 typedef float real32;
 typedef double real64;
+
+typedef float f32;
+typedef double d64;
+
+typedef bool b32;
+
+typedef intptr_t intptr;
+typedef uintptr_t uintptr;
+
+typedef size_t memory_index;
+
+typedef uintptr_t umm;
+typedef intptr_t smm;
 
 union v2 {
     struct
