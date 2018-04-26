41485e3dfa9bf849c7a201f71aac88a352bac818
Marbin Tan <ayamaru@gmail.com>
2018-04-25 23:48:23 -0700

    build.bat: fix -set env before checking arguments.
    I was setting the environment variables needed to build the renderer in
    the main build path. If I never ran the main build path in the current
    command prompt and then ran the render build. I will get a linker
    failure.
    
    Fix: put the LIB_PATH above the check so that it always get set.

--- build.bat
+++ build.bat
@@ -1,11 +1,5 @@
 @echo off
-if "%1" == "" (
-    goto :top
-) ELSE (
-    goto :update_and_render
-)
 
-:top
 SET ORIGINAL_DIR=%cd%
 SET LIBS=kernel32.lib User32.lib Winmm.lib Gdi32.lib OpenGL32.lib glu32.lib 
 SET LIBS=%LIBS% glew32.lib SDL2.lib SDL2main.lib SDL2_ttf.lib
@@ -19,6 +13,13 @@ SET LIB_PATH=/LIBPATH:D:\Libraries\SDL2-2.0.7\VisualC\x64\Release %LIB_PATH%
 SET LIB_PATH=/LIBPATH:D:\Libraries\SDL2-2.0.7\VisualC\x64\Release %LIB_PATH%
 SET LIB_PATH=/LIBPATH:D:\Libraries\SDL2_ttf-2.0.14\VisualC\x64\Debug %LIB_PATH%
 
+if "%1" == "" (
+    goto :top
+) ELSE (
+    goto :update_and_render
+)
+
+:top
 SET FILES=..\game\main.cpp
 
 REM TODO: we'll need to port over glew32.dll and SDL2.dll SDL2_ttf.dll
@@ -41,7 +42,6 @@ SET INCLUDE_PATH=/I D:\Libraries\SDL2_ttf-2.0.14 %INCLUDE_PATH%
 cl /DWIN32#1 %COMPILER_FLAGS% -I..\iaca-win64\ %INCLUDE_PATH% render.cpp -Fmrender.map -LD /link %LIB_PATH% %LINKER_FLAGS% -incremental:no -opt:ref -PDB:game_%random%.pdb -EXPORT:Render -EXPORT:UpdateAndRender -OUT:render_new.dll 
 
 copy render_new.dll render.dll
-
 
 rem copy %ORIGINAL_DIR%\*.glsl .
 rem copy %ORIGINAL_DIR%\*.vs .
