4a4a1272ec21915469ee283becdf44d1190f33bc
Marbin Tan <ayamaru@gmail.com>
2018-05-17 15:33:09 -0700

    Missed render_group.h... WOOPS

---
+++ render_group.h
@@ -0,0 +1,25 @@
+#ifndef __RENDER_GROUP__
+#define __RENDER_GROUP__
+
+#include "rectangle.h"
+
+struct RenderGroup
+{
+    GameMemory vertexMemory;
+    memory_index rectCount;
+};
+
+inline void ClearUsedRenderGroup(RenderGroup *rg)
+{
+    rg->rectCount = 0;
+    ClearMemoryUsed(&rg->vertexMemory);
+};
+
+inline void PushRect(RenderGroup *rg, Rect *rect)
+{
+    PushRect(&rg->vertexMemory, rect);
+    rg->rectCount++;
+}
+
+
+#endif
