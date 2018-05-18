45e39dd3d51d8bb17ffcb56f74368fc579d71122
Jane Lusby <jlusby42@gmail.com>
2018-05-17 17:34:52 -0700

    Rename docker build script and allow arg passing

--- run.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-docker run --rm -it -v"$(pwd)":/build:Z jobless:testing /bin/sh -c "\
-    cd /build || exit 1; \
-    cmake -D USE_OPENGL_ES=1 .; \
-    make; \
-    chown -R $(stat -c "%u:%g" .) /build \
-    "
