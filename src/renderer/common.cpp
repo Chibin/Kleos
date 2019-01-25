2c8bf739fff4b33a2261e2e8adc73aa76b8718b1
Marbin Tan <ayamaru@gmail.com>
2019-01-25 11:34:11 -0800

    Fix correction matrix
    I was putting the 0.5 value on the wrong matrix.

--- src/renderer/common.cpp
+++ src/renderer/common.cpp
@@ -56,7 +56,7 @@ void UpdateUBOandPushConstants(
         glm::mat4 correction = glm::mat4(1.0);
         correction[1][1] = -1;
         correction[2][2] = 0.5;
-        correction[2][3] = 0.5;
+        correction[3][2] = 0.5;
 
         pushConstants.proj = correction * (*g_projection);
         pushConstants.view = g_camera->view;
