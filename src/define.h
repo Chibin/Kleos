51a10960d301a3a4d287b1d804547971a7e1c712
Marbin Tan <ayamaru@gmail.com>
2019-04-25 16:03:25 -0700

    Refactor: move color macro
    Put into the define.h macros

--- src/define.h
+++ src/define.h
@@ -1,1 +1,33 @@
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+
+#define COLOR_WHITE \
+    v4              \
+    {               \
+        1, 1, 1, 1  \
+    }
+#define COLOR_RED  \
+    v4             \
+    {              \
+        1, 0, 0, 1 \
+    }
+#define COLOR_GREEN \
+    v4              \
+    {               \
+        0, 1, 0, 1  \
+    }
+#define COLOR_BLUE \
+    v4             \
+    {              \
+        0, 0, 1, 1 \
+    }
+#define COLOR_BLACK \
+    v4              \
+    {               \
+        0, 0, 0, 1  \
+    }
+
+#define COLOR_YELLOW\
+    v4              \
+    {               \
+        1, 1, 0, 1  \
+    }
--- src/render.cpp
+++ src/render.cpp
@@ -83,37 +83,6 @@ inline void *RequestToReservedMemory(memory_index size)
 #include "renderer/vulkan/my_vulkan.cpp"
 #include "renderer/common.cpp"
 
-#define COLOR_WHITE \
-    v4              \
-    {               \
-        1, 1, 1, 1  \
-    }
-#define COLOR_RED  \
-    v4             \
-    {              \
-        1, 0, 0, 1 \
-    }
-#define COLOR_GREEN \
-    v4              \
-    {               \
-        0, 1, 0, 1  \
-    }
-#define COLOR_BLUE \
-    v4             \
-    {              \
-        0, 0, 1, 1 \
-    }
-#define COLOR_BLACK \
-    v4              \
-    {               \
-        0, 0, 0, 1  \
-    }
-
-#define COLOR_YELLOW\
-    v4              \
-    {               \
-        1, 1, 0, 1  \
-    }
 #include "random.cpp"
 #include "npc.cpp"
 
