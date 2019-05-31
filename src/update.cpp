28a277c6201146bee5e5757da5923ecc77e63cb8
Marbin Tan <ayamaru@gmail.com>
2019-05-30 20:12:06 -0700

    Add macro for_each_hash_key_val
    This will enable us to loop through all the values with-in a hash.
    Replaced hardcoded value in the update entities to all entities.

--- src/update.cpp
+++ src/update.cpp
@@ -18,222 +18,231 @@ void UpdateEntities(GameMetadata *gameMetadata, GameTimestep *gt, RectDynamicArr
 
     f32 dt = gt->dt;
 
-    Entity *e = gameMetadata->playerEntity;
-    Movement *movement = HashGetValue(HashEntityMovement, gameMetadata->hashEntityMovement, e);
-    Rect *rectFromEntity = HashGetValue(HashEntityRect, gameMetadata->hashEntityRect, e);
+    FOR_EACH_HASH_KEY_VAL_BEGIN(HashEntityRect, hashKeyVal, gameMetadata->hashEntityRect)
+    {
+        Entity *e = hashKeyVal->key;
+        Rect *rectFromEntity = hashKeyVal->val;
+        Movement *movement = HashGetValue(HashEntityMovement, gameMetadata->hashEntityMovement, e);
 
-    //      if (IntersectionAABB(rect, v2{e->position.x, e->position.y},
-    //      rayDirection))
-    v2 center = 0.5f * v2{rectFromEntity->width, rectFromEntity->height};
-    Rect nextUpdate = *rectFromEntity;
-    nextUpdate.min = V2(movement->position) + V2(movement->velocity) * dt + 0.5f * V2(movement->acceleration) * dt * dt - center;
-    nextUpdate.max = V2(movement->position) + V2(movement->velocity) * dt + 0.5f * V2(movement->acceleration) * dt * dt + center;
+        //Entity *e = gameMetadata->playerEntity;
+        //Movement *movement = HashGetValue(HashEntityMovement, gameMetadata->hashEntityMovement, e);
+        //Rect *rectFromEntity = HashGetValue(HashEntityRect, gameMetadata->hashEntityRect, e);
 
-    /* XXX: If we're on the ground we should always reset the accelration to the force of gravity. */
-    movement->acceleration.y += gravity;
+        //      if (IntersectionAABB(rect, v2{e->position.x, e->position.y},
+        //      rayDirection))
+        v2 center = 0.5f * v2{rectFromEntity->width, rectFromEntity->height};
+        Rect nextUpdate = *rectFromEntity;
+        nextUpdate.min = V2(movement->position) + V2(movement->velocity) * dt + 0.5f * V2(movement->acceleration) * dt * dt - center;
+        nextUpdate.max = V2(movement->position) + V2(movement->velocity) * dt + 0.5f * V2(movement->acceleration) * dt * dt + center;
 
-    MinMax temp = GetMinMax(&nextUpdate);
-    AABB range = MinMaxToSquareAABB(&temp);
+        /* XXX: If we're on the ground we should always reset the accelration to the force of gravity. */
+        movement->acceleration.y += gravity;
+
+        MinMax temp = GetMinMax(&nextUpdate);
+        AABB range = MinMaxToSquareAABB(&temp);
 #if 1
-    f32 arbitraryPadding = 5.0f;
+        f32 arbitraryPadding = 5.0f;
 #else
-    f32 arbitraryPadding = 50.0f;
+        f32 arbitraryPadding = 50.0f;
 #endif
-    range.halfDim = range.halfDim + arbitraryPadding;
-    Rect **arr = GetRectsWithInRange(gameMetadata->sm, &range);
-    AddDebugRect(gameMetadata, &range, COLOR_GREEN_TRANSPARENT);
+        range.halfDim = range.halfDim + arbitraryPadding;
+        Rect **arr = GetRectsWithInRange(gameMetadata->sm, &range);
+        AddDebugRect(gameMetadata, &range, COLOR_GREEN_TRANSPARENT);
 
-    AABB uiTest = {};
-    glm::vec3 rayWorld =
-        UnProject(gameMetadata->camera, gameMetadata->projection, V2(g_mousePoint), gameMetadata->screenResolution);
+        AABB uiTest = {};
+        glm::vec3 rayWorld =
+            UnProject(gameMetadata->camera, gameMetadata->projection, V2(g_mousePoint), gameMetadata->screenResolution);
 
-    f32 dimRange = 0.1f;
-    uiTest.halfDim = v2{dimRange, dimRange * (gameMetadata->screenResolution.x / gameMetadata->screenResolution.y)};
-    uiTest.center = V2(ScreenSpaceToNormalizedDeviceSpace(V2(g_mousePoint), gameMetadata->screenResolution));
-    AddDebugRectUI(gameMetadata, &uiTest, COLOR_RED_TRANSPARENT);
+        f32 dimRange = 0.1f;
+        uiTest.halfDim = v2{dimRange, dimRange * (gameMetadata->screenResolution.x / gameMetadata->screenResolution.y)};
+        uiTest.center = V2(ScreenSpaceToNormalizedDeviceSpace(V2(g_mousePoint), gameMetadata->screenResolution));
+        AddDebugRectUI(gameMetadata, &uiTest, COLOR_RED_TRANSPARENT);
 
-    /* This points towards us */
-    glm::vec3 infinitePlaneNormal = glm::vec3(0, 0, 1);
+        /* This points towards us */
+        glm::vec3 infinitePlaneNormal = glm::vec3(0, 0, 1);
 
-    glm::vec3 worldPos =
-        GetWorldPointFromMouse(
-                gameMetadata->camera,
-                gameMetadata->projection,
-                V2(g_mousePoint),
-                gameMetadata->screenResolution,
-                infinitePlaneNormal);
+        glm::vec3 worldPos =
+            GetWorldPointFromMouse(
+                    gameMetadata->camera,
+                    gameMetadata->projection,
+                    V2(g_mousePoint),
+                    gameMetadata->screenResolution,
+                    infinitePlaneNormal);
 
-    uiTest.halfDim = v2{dimRange, dimRange};
-    uiTest.center = v2{worldPos.x, worldPos.y};
+        uiTest.halfDim = v2{dimRange, dimRange};
+        uiTest.center = v2{worldPos.x, worldPos.y};
 #if 0
-    printf("coords: X %f, Y %f\n", uiTest.center.x, uiTest.center.y);
+        printf("coords: X %f, Y %f\n", uiTest.center.x, uiTest.center.y);
 #endif
 
-    AddDebugRect(gameMetadata, &uiTest, COLOR_BLUE_TRANSPARENT);
+        AddDebugRect(gameMetadata, &uiTest, COLOR_BLUE_TRANSPARENT);
 
-    for(memory_index i = 0; i < ARRAY_LIST_SIZE(arr); i++)
-    {
-        Rect *rect = arr[i];
-        AddDebugRect(gameMetadata, rect, COLOR_YELLOW_TRANSPARENT);
-
-        glm::vec3 rayDirection = glm::vec3(1 / rayWorld.x, 1 / rayWorld.y, 0);
-        if (IntersectionAABB(rect, V2(gameMetadata->camera->pos), rayDirection))
+        for(memory_index i = 0; i < ARRAY_LIST_SIZE(arr); i++)
         {
-            AddDebugRect(gameMetadata, rect, COLOR_BLACK_TRANSPARENT);
-        }
-#if 0
-        real32 dX = e->velocity.x * dt;
-        real32 dY = e->velocity.y + (gravity + e->acceleration.y) * dt;
+            Rect *rect = arr[i];
+            AddDebugRect(gameMetadata, rect, COLOR_YELLOW_TRANSPARENT);
 
-        glm::vec3 rayDirection = { dX, dY, 0 };
-        rayDirection = glm::normalize(rayDirection);
-        rayDirection = glm::vec3(1 / rayDirection.x, 1 / rayDirection.y, 0);
-#endif
-        if (rect->type == COLLISION && TestAABBAABB(rect, &nextUpdate))
-        {
-            /* how to differentiate between x and y axis? */
-            movement->position.y = rect->max.y + rectFromEntity->height * 0.5f;
-            movement->velocity.y = 0;
-            movement->acceleration.y = gravity;
-        }
-    }
-
-    /* TODO: There is a bug here. We're not properly updating the position
-     * based on the collisions
-     */
-    movement->position += movement->velocity * dt + 0.5f * movement->acceleration * dt * dt;
-    movement->velocity.y += movement->acceleration.y *dt;
-
-    UpdatePositionBasedOnCollission(gameMetadata->sm, g_enemyNPC, gravity, dt);
-
-    /* TODO: can't let the player spam attack while we're still in an attack animation */
-    if (e->willAttack)
-    {
-        /* This is related to hit boxes of the frames */
-        if (e->frameState.startFrame == NULL)
-        {
-            e->frameState.startFrame =
-                (FrameData *)AllocateMemory(reservedMemory, sizeof(FrameData));
-
-            e->frameState.startFrame->duration = 170;
-            e->frameState.startFrame->dim = v2{0.35f, 0.175f};
-            e->frameState.startFrame->pos = v3{1.5, 0, 0};
-            e->frameState.startFrame->next = NULL;
-
-            e->frameState.currentFrame = e->frameState.startFrame;
-
-            /* TODO: This should be somewhere else */
-            e->frameState.transform = glm::mat4(-2.0f);
-            e->frameState.transform[3][3] = 1.0f;
-
-            if (rectFromEntity->frameDirection == RIGHT)
+            glm::vec3 rayDirection = glm::vec3(1 / rayWorld.x, 1 / rayWorld.y, 0);
+            if (IntersectionAABB(rect, V2(gameMetadata->camera->pos), rayDirection))
             {
-                e->frameState.transform[0][0] = abs(e->frameState.transform[0][0]);
-                e->frameState.transform[1][1] = abs(e->frameState.transform[1][1]);
+                AddDebugRect(gameMetadata, rect, COLOR_BLACK_TRANSPARENT);
+            }
+#if 0
+            real32 dX = e->velocity.x * dt;
+            real32 dY = e->velocity.y + (gravity + e->acceleration.y) * dt;
+
+            glm::vec3 rayDirection = { dX, dY, 0 };
+            rayDirection = glm::normalize(rayDirection);
+            rayDirection = glm::vec3(1 / rayDirection.x, 1 / rayDirection.y, 0);
+#endif
+            if (rect->type == COLLISION && TestAABBAABB(rect, &nextUpdate))
+            {
+                /* how to differentiate between x and y axis? */
+                movement->position.y = rect->max.y + rectFromEntity->height * 0.5f;
+                movement->velocity.y = 0;
+                movement->acceleration.y = gravity;
+            }
+        }
+
+        /* TODO: There is a bug here. We're not properly updating the position
+         * based on the collisions
+         */
+        movement->position += movement->velocity * dt + 0.5f * movement->acceleration * dt * dt;
+        movement->velocity.y += movement->acceleration.y *dt;
+
+        UpdatePositionBasedOnCollission(gameMetadata->sm, g_enemyNPC, gravity, dt);
+
+        /* TODO: can't let the player spam attack while we're still in an attack animation */
+        if (e->willAttack)
+        {
+            /* This is related to hit boxes of the frames */
+            if (e->frameState.startFrame == NULL)
+            {
+                e->frameState.startFrame =
+                    (FrameData *)AllocateMemory(reservedMemory, sizeof(FrameData));
+
+                e->frameState.startFrame->duration = 170;
+                e->frameState.startFrame->dim = v2{0.35f, 0.175f};
+                e->frameState.startFrame->pos = v3{1.5, 0, 0};
+                e->frameState.startFrame->next = NULL;
+
+                e->frameState.currentFrame = e->frameState.startFrame;
+
+                /* TODO: This should be somewhere else */
+                e->frameState.transform = glm::mat4(-2.0f);
+                e->frameState.transform[3][3] = 1.0f;
+
+                if (rectFromEntity->frameDirection == RIGHT)
+                {
+                    e->frameState.transform[0][0] = abs(e->frameState.transform[0][0]);
+                    e->frameState.transform[1][1] = abs(e->frameState.transform[1][1]);
+                }
+
             }
 
+            if (e->frameState.timePassedCurrentFrame == 0)
+            {
+                rectFromEntity->spriteAnimation =
+                    GetSpriteAnimationInfo(
+                            GetFrameAnimation(&gameMetadata->frameAnimationSentinelNode, "arche.png"),
+                            "ATTACK");
+            }
+
+            //AttackInfo attackInfo = GetAttackFrameInfo();
+            if (UpdateFrameState(&e->frameState, gt->deltaTime))
+            {
+                /* TODO: The position that's passed needs to be the center of the
+                 * movement. Otherwise, it's hard to make the frame distance
+                 * symetrical when swapping between both directions
+                 */
+                AddDebugRect(gameMetadata, &e->frameState, V3(movement->position), COLOR_RED_TRANSPARENT);
+            }
+            else
+            {
+                e->willAttack = false;
+            }
         }
 
-        if (e->frameState.timePassedCurrentFrame == 0)
+        if (rectFromEntity->spriteAnimation->currentFrameIndex == 0 && e->willAttack == false)
         {
             rectFromEntity->spriteAnimation =
                 GetSpriteAnimationInfo(
                         GetFrameAnimation(&gameMetadata->frameAnimationSentinelNode, "arche.png"),
-                        "ATTACK");
+                        "IDLE");
         }
 
-        //AttackInfo attackInfo = GetAttackFrameInfo();
-        if (UpdateFrameState(&e->frameState, gt->deltaTime))
+        for (int i = 0; i < hitBoxes->size; i++)
         {
-            /* TODO: The position that's passed needs to be the center of the
-             * movement. Otherwise, it's hard to make the frame distance
-             * symetrical when swapping between both directions
-             */
-            AddDebugRect(gameMetadata, &e->frameState, V3(movement->position), COLOR_RED_TRANSPARENT);
-        }
-        else
-        {
-            e->willAttack = false;
-        }
-    }
-
-    if (rectFromEntity->spriteAnimation->currentFrameIndex == 0 && e->willAttack == false)
-    {
-        rectFromEntity->spriteAnimation =
-            GetSpriteAnimationInfo(
-                    GetFrameAnimation(&gameMetadata->frameAnimationSentinelNode, "arche.png"),
-                    "IDLE");
-    }
-
-    for (int i = 0; i < hitBoxes->size; i++)
-    {
-        Rect *rect = hitBoxes->rects[i];
-        for (int y = 0; y < hurtBoxes->size; y++)
-        {
-            Rect hb = *(hurtBoxes->rects[y]);
-            if (TestAABBAABB(rect, &hb))
+            Rect *rect = hitBoxes->rects[i];
+            for (int y = 0; y < hurtBoxes->size; y++)
             {
-                printf("CHECKING hitboxes\n");
+                Rect hb = *(hurtBoxes->rects[y]);
+                if (TestAABBAABB(rect, &hb))
+                {
+                    printf("CHECKING hitboxes\n");
+                }
             }
         }
+
+        if (isPlayer)
+        {
+            /* TODO: bound checking for the camera such that we only move the camera
+             * when necessary
+             */
+            //UpdateEntityFrameDirection();
+            if (movement->velocity.x > 0)
+            {
+                rectFromEntity->frameDirection = RIGHT;
+                e->frameState.transform[0][0] = abs(e->frameState.transform[0][0]);
+                e->frameState.transform[1][1] = abs(e->frameState.transform[1][1]);
+            }
+            else if (movement->velocity.x < 0)
+            {
+                rectFromEntity->frameDirection = LEFT;
+                e->frameState.transform[0][0] = -1 * abs(e->frameState.transform[0][0]);
+                e->frameState.transform[1][1] = -1 * abs(e->frameState.transform[1][1]);
+            }
+            /* else don't update */
+
+            /* follow the character around */
+            CameraUpdateTarget(gameMetadata->camera, movement->position);
+            UpdatePosition(rectFromEntity, V3(movement->position));
+
+            UpdateCurrentFrame(rectFromEntity->spriteAnimation, 17.6f);
+            UpdateUV(rectFromEntity, *rectFromEntity->spriteAnimation->currentFrame);
+            UpdateFrameDirection(rectFromEntity->spriteAnimation, rectFromEntity->frameDirection);
+        }
+
+        /* Apply "friction" */
+        movement->velocity.x = 0;
+
+        for (memory_index i = 0; i < gameMetadata->particleSystem.particleCount; i++)
+        {
+            Particle *particle = &gameMetadata->particleSystem.particles[i]; // NOLINT
+
+            v3 newPosition = V3(movement->position);
+
+            particle->acc.y = gravity * 2;
+            particle->vel.x = 0;
+            UpdateParticlePosition(particle, dt);
+            if (particle->positionOffset.y <= rectFromEntity->min.y)
+            {
+                particle->positionOffset.y = newPosition.y;
+                particle->acc.y = 0;
+                UpdatePosition(&particle->rect, particle->positionOffset);
+            }
+            else
+            {
+                UpdatePosition(&particle->rect, particle->positionOffset + newPosition);
+            }
+
+        }
+        UpdateAndGenerateParticleRectInfo(perFrameRenderGroup,
+                &gameMetadata->particleSystem,
+                (s32)gameMetadata->gameTimestep->deltaTime);
+
     }
-
-    if (isPlayer)
-    {
-        /* TODO: bound checking for the camera such that we only move the camera
-         * when necessary
-         */
-        //UpdateEntityFrameDirection();
-        if (movement->velocity.x > 0)
-        {
-            rectFromEntity->frameDirection = RIGHT;
-            e->frameState.transform[0][0] = abs(e->frameState.transform[0][0]);
-            e->frameState.transform[1][1] = abs(e->frameState.transform[1][1]);
-        }
-        else if (movement->velocity.x < 0)
-        {
-            rectFromEntity->frameDirection = LEFT;
-            e->frameState.transform[0][0] = -1 * abs(e->frameState.transform[0][0]);
-            e->frameState.transform[1][1] = -1 * abs(e->frameState.transform[1][1]);
-        }
-        /* else don't update */
-
-        /* follow the character around */
-        CameraUpdateTarget(gameMetadata->camera, movement->position);
-        UpdatePosition(rectFromEntity, V3(movement->position));
-
-        UpdateCurrentFrame(rectFromEntity->spriteAnimation, 17.6f);
-        UpdateUV(rectFromEntity, *rectFromEntity->spriteAnimation->currentFrame);
-        UpdateFrameDirection(rectFromEntity->spriteAnimation, rectFromEntity->frameDirection);
-    }
-
-    /* Apply "friction" */
-    movement->velocity.x = 0;
-
-    for (memory_index i = 0; i < gameMetadata->particleSystem.particleCount; i++)
-    {
-        Particle *particle = &gameMetadata->particleSystem.particles[i]; // NOLINT
-
-        v3 newPosition = V3(movement->position);
-
-        particle->acc.y = gravity * 2;
-        particle->vel.x = 0;
-        UpdateParticlePosition(particle, dt);
-        if (particle->positionOffset.y <= rectFromEntity->min.y)
-        {
-            particle->positionOffset.y = newPosition.y;
-            particle->acc.y = 0;
-            UpdatePosition(&particle->rect, particle->positionOffset);
-        }
-        else
-        {
-            UpdatePosition(&particle->rect, particle->positionOffset + newPosition);
-        }
-
-    }
-    UpdateAndGenerateParticleRectInfo(perFrameRenderGroup,
-                                      &gameMetadata->particleSystem,
-                                      (s32)gameMetadata->gameTimestep->deltaTime);
+    FOR_EACH_HASH_KEY_VAL_END();
 }
 
 void UpdateBasedOnEditModeChanges(GameMetadata *gameMetadata)
